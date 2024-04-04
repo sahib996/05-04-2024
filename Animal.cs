5ccab3cdc0c3ebc88f01d5ded028c71a6de4d300
sahib memmedov <sahibmemmedov7890@gmail.com>
2024-04-04 14:20:53 -0700

    initial commit

---
+++ Animal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstarct
+{
+    internal  abstract class Animal
+    {
+        public abstract void Eat();
+        
+    }
+    abstract class Bird: Animal
+    {
+        public abstract  void Fly();
+        
+    }
+    class Parrot: Bird
+    {
+        public override  void Eat()
+        {
+            Console.WriteLine("Eat as Parrot");
+        }
+    }
+    public override void Fly()
+    {
+        Console.WriteLine("Flay as Parrot");
+    }
+    public void Talk()
+    {
+        Console.WriteLine("Cici kush");
+    }
+    class Eagle: Bird
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Eat as Eagle");
+        }
+        public override void Fly()
+        {
+            Console.WriteLine("Fly ad Eagle");
+        }
+    abstract class Fish : Animal
+    {
+        public void Swim()
+        {
+            Console.WriteLine("Swim");
+        }
+    }
+    class Shark :Fish
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Eat as Sharks");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Abstarct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parrot b = new Parrot();
+            Shark akula = new Shark();
+            Eagle eagle = new Eagle();
+            Animal[] animals = {b, akula, eagle };
+            Bird[] birds = {b, , eagle};
+            /*b.Eat();
+            akula.Eat();
+            eagle.Fly();*/
+            foreach(var animal in animals)
+            {
+                bird.Fly();
+            }
+
+        }
+    }
+}
